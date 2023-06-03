404dcc57c9d26280edde3644d1d3b31fd1322273
Asif Bhat <theasifbhat@gmail.com>
2023-06-03 22:15:39 +0530

    added headless mode

--- src/test/java/testingComponents/BaseTest.java
+++ src/test/java/testingComponents/BaseTest.java
@@ -7,8 +7,11 @@ import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import pageObjects.LandingPage;
@@ -46,19 +49,40 @@ public class BaseTest {
                 WebDriverManager.firefoxdriver().setup();
                 System.out.println("firefox is selected");
                 mDriver = new FirefoxDriver();
+            }
+            case "firefoxHeadless" -> {
+                WebDriverManager.firefoxdriver().setup();
+                System.out.println("firefox headless is selected");
+                FirefoxOptions options = new FirefoxOptions();
+                options.addArguments("--headless");
+                mDriver = new FirefoxDriver(options);
                 mDriver.manage().window().setSize(new Dimension(1440,900));
             }
             case "edge" -> {
                 WebDriverManager.edgedriver().setup();
                 System.out.println("edge is selected");
                 mDriver = new EdgeDriver();
+            }
+            case "edgeHeadless" -> {
+                WebDriverManager.edgedriver().setup();
+                System.out.println("edge headless is selected");
+                EdgeOptions options = new EdgeOptions();
+                options.addArguments("--headless");
+                mDriver = new EdgeDriver(options);
+                mDriver.manage().window().setSize(new Dimension(1440,900));
+            }
+            case "chromeHeadless" -> {
+                WebDriverManager.chromedriver().setup();
+                System.out.println("chrome is selected");
+                ChromeOptions options = new ChromeOptions();
+                options.addArguments("--headless");
+                mDriver = new ChromeDriver(options);
                 mDriver.manage().window().setSize(new Dimension(1440,900));
             }
             default -> {
                 WebDriverManager.chromedriver().setup();
                 System.out.println("default/chrome is selected");
                 mDriver = new ChromeDriver();
-                mDriver.manage().window().setSize(new Dimension(1440,900));
             }
         }
 
